9e1465e89e1ba371793988fd13426cd774ce3ae2
Ayush Jain <j.ayush790@gmail.com>
2023-08-01 22:15:01 +0530

    final commit

---
+++ backend/db.js
@@ -0,0 +1,34 @@
+// const require = createRequire(import.meta.url);
+const mongoose = require("mongoose");
+
+const mongoURI = 'mongodb+srv://gofood:gofood123@cluster0.zpusfds.mongodb.net/gofoodmern?retryWrites=true&w=majority';
+
+const mongoDB = async () => {
+  await mongoose.connect(mongoURI,{useNewUrlParser:true},async(err,result)=>{
+        if(err) console.log("...",err)
+        else{
+                   console.log("connected");
+                   const fetched_data=await mongoose.connection.db.collection("food_items");
+                   fetched_data.find({}).toArray(async function(err,data){
+                    const foodCategory=await mongoose.connection.db.collection("foodCategory");
+                   foodCategory.find({}).toArray(function(err,catData){
+                        if(err) console.log(err);
+                        else{
+                          global.fooditems=data;
+                          global.food_category=catData;
+                        }
+
+
+                   })
+                    // if(err) console.log(err);
+                    // else {
+                    
+                    //     global.food_items=data;
+                        
+                    // }
+                   })
+        }
+  });
+}  
+
+module.exports = mongoDB;
